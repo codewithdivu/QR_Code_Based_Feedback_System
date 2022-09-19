137a4931295924a2a16515968a66ca7c4c29b9d7
alwaysalazycoder <gs314315316@gmail.com>
2022-09-19 21:38:15 +0530

    changed in app.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ const App = () => {
       <h1>hello</h1>
       <p>hello my self hemant bhadarka now i am collaborator of this project</p>
       <h1> hello </h1>
+      <p>
+        hello guys..
+      </p>
     </>
   );
 };
