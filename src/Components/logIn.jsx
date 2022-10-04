9cd9091dbea4f3cb0fb1cdc4c6d3be5dc38c87cf
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-10-04 21:19:20 +0530

    ....

--- src/Components/logIn.jsx
+++ src/Components/logIn.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const LogIn = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    setPhoneNumber(data.mobile_num);
+  };
+
   return (
     <div className="main_container">
       <div className="police_logo_details_container">
@@ -17,29 +31,41 @@ const LogIn = () => {
             <hr />
             <p>Kindly enter your mobile number to authenticate yourself.</p>
           </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form_data">
+              <div className="mobile_num_label">
+                <label for="mobile_num" className="mobile_num">
+                  Enter mobile no :
+                </label>
+              </div>
+              <div className="mobile_num_input">
+                <input
+                  type="number"
+                  name="mobile_num"
+                  id=""
+                  className="mobile_num"
+                  {...register("mobile_num", { required: true, maxLength: 10 })}
+                />
+                {errors.mobile_num && <p>Please check the Mobile Number</p>}
+              </div>
+              <div className="otp_label">Enter OTP :</div>
+              <div className="otp_input">
+                <input
+                  type="number"
+                  className="otp"
+                  name="otp"
+                  id=""
+                  {...register("otp", { required: true })}
+                />
+                {errors.otp && <p>Please check the OTP</p>}
+              </div>
 
-          <div className="form_data">
-            <div className="mobile_num_label">
-              <label for="mobile_num" className="mobile_num">
-                Enter mobile no :
-              </label>
+              <button className="sendOtp">Send OTP</button>
+              <button type="submit" className="submit_btn">
+                Next
+              </button>
             </div>
-            <div className="mobile_num_input">
-              <input
-                type="number"
-                name="mobile_num"
-                id=""
-                className="mobile_num"
-              />
-            </div>
-            <div className="otp_label">Enter OTP :</div>
-            <div className="otp_input">
-              <input type="number" className="otp" name="otp" id="" />
-            </div>
-
-            <button className="sendOtp">Send OTP</button>
-            <button className="submit_btn">Next</button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
