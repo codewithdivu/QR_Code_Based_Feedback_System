819501a21c0e8d534a2d4739d4dcf3ef1b892aef
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-10-04 13:05:16 +0530

    ..........

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import ChooseLanguage from "./Components/chooseLanguage";
+import FeedbackQuestion1 from "./Components/feedbackQuestion1";
+import FeedbackQuestion2 from "./Components/feedbackQuestion2";
+import FeedbackQuestion3 from "./Components/feedbackQuestion3";
+import LogIn from "./Components/logIn";
+import SelectStation from "./Components/selectStation";
 
 const App = () => {
   return (
     <>
-    
+      <Routes>
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/language" element={<ChooseLanguage />} />
+        <Route path="/station" element={<SelectStation />} />
+        <Route path="/feedback1" element={<FeedbackQuestion1 />} />
+        <Route path="/feedback2" element={<FeedbackQuestion2 />} />
+        <Route path="/feedback3" element={<FeedbackQuestion3 />} />
+      </Routes>
     </>
   );
 };
--- src/Components/logIn.jsx
+++ src/Components/logIn.jsx
@@ -1,28 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const LogIn = () => {
   return (
     <div class="main">
-    <img class="police__img" src="./images/Logo.png" alt="">
-    <span class="police__heading">GUJARAT POLICE</span>
-    <span class="police__feedback">(FEEDBACK PORTAL)</span>
-    <form class="police__num">
-        <div><input class="police__num1" placeholder="Enter Your Mobile Number..." type="number" pattern="[0-9] {10}"></div>
-        <div class="policeotp"><button class="police__otp">Send OTP</button></div>
-
-        <div class="police__scnd"><input class="policeotp1" placeholder="Enter OTP..." type="number"></div>
-        <div class="policeotp "><button class="police__otp policevrfy">Verify</button></div>
-
-        <div class= "policenext1">
-            <button class="policenext">NEXT</button>
+      <img class="police__img" src="./images/Logo.png" alt="" />
+      <span class="police__heading">GUJARAT POLICE</span>
+      <span class="police__feedback">(FEEDBACK PORTAL)</span>
+      <form class="police__num">
+        <div>
+          <input
+            class="police__num1"
+            placeholder="Enter Your Mobile Number..."
+            type="number"
+            pattern="[0-9] {10}"
+          />
+        </div>
+        <div class="policeotp">
+          <button class="police__otp">Send OTP</button>
         </div>
 
+        <div class="police__scnd">
+          <input class="policeotp1" placeholder="Enter OTP..." type="number" />
+        </div>
+        <div class="policeotp ">
+          <button class="police__otp policevrfy">Verify</button>
+        </div>
 
-
-    </form>
-   
+        <div class="policenext1">
+          <button class="policenext">NEXT</button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default LogIn
+export default LogIn;
