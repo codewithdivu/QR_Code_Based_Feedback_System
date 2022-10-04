be076817a44afa70f4a9e1391f25cc43c785e295
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-10-04 22:26:34 +0530

    ....

--- src/Components/selectStation.jsx
+++ src/Components/selectStation.jsx
@@ -5,7 +5,7 @@ const SelectStation = () => {
     <div className="main_container">
       <div className="police_logo_details_container">
         <div className="police_logo">
-          <img src="./Logo.png" alt="GUJ_POLICE_LOGO" />
+          <img src="./images/Logo.png" alt="GUJ_POLICE_LOGO" />
         </div>
         <div className="police_heading">GUJARAT POLICE</div>
         <div className="police_feedback">(Feedback Portal)</div>
