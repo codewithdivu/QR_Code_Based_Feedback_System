c4dcf46198b61777ef5917df70e20a0aefa6a6d6
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-10-11 00:07:26 +0530

    added some validation in date pickers in admin panel

--- src/Components/Admin/content.jsx
+++ src/Components/Admin/content.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import Table from "../../Assets/table";
-import { newData } from "../../constants/districts";
+import { dk, newData } from "../../constants/districts";
 import { collections } from "../../firebase/collections";
 import { useFireStore } from "../../hooks";
 import Navbar from "./adminPortal/navbar";
@@ -12,19 +13,8 @@ const Content = () => {
   const [taluka, setTaluka] = useState("");
   const [station, setStation] = useState("");
 
-  // Handlers Methods
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!district || !taluka) {
-      return alert("required...");
-    }
-    // onSelectStation({
-    //   district: district,
-    //   taluka: taluka,
-    //   police: station,
-    // });
-    // onNext();
-  };
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
 
   return (
     <>
@@ -58,160 +48,101 @@ const Content = () => {
               </div>
             </div>
           </div>
-          {/* return array.filter((v) => v === value).length */}
           <div className="activity">
             <div className="title">
               <i className="fa fa-solid fa-people-group"></i>
               <span className="text">Select police station</span>
             </div>
 
-            <div className="select__police">
-              {/* <div className="select__distic">
-                <form className="district">
-                  <select className="district__1">
-                    <option value="0" className="option__forform" selected="">
-                      Select District...
+            {/* here will come dropdown  */}
+            <div>
+              <div className="district">
+                <select
+                  className="district__1"
+                  value={district}
+                  onChange={(e) => setDistrict(e.target.value)}
+                  // disabled={selectedStation}
+                >
+                  <option value="">Select District</option>
+                  {dk.map((dist) => (
+                    <option value={dist.value} className="option__forform">
+                      {dist.label}
                     </option>
-                    <option value="1" className="option__forform">
-                      DEVBHUMI DWARKA
-                    </option>
-                    <option value="2" className="option__forform">
-                      SURAT
-                    </option>
-                    <option value="3" className="option__forform">
-                      RAJKOT
-                    </option>
-                    <option value="4" className="option__forform">
-                      PORBANDAR
-                    </option>
-                    <option value="5" className="option__forform">
-                      JAMANAGAR
-                    </option>
-                    <option value="6" className="option__forform">
-                      JUNAGADH
-                    </option>
-                    <option value="7" className="option__forform">
-                      AMRELI
-                    </option>
-                  </select>
-                </form>
+                  ))}
+                </select>
               </div>
-              <div className="select__taluko">
-                <form className="district__taluko">
-                  <select className="district__taluko1">
-                    <option value="0" className="option__forform" selected="">
-                      Select Taluka...
-                    </option>
-                    <option value="1" className="option__forform">
-                      KALYANPUR
-                    </option>
-                    <option value="2" className="option__forform">
-                      OKHA
-                    </option>
-                    <option value="3" className="option__forform">
-                      KHAMBHALIYA
-                    </option>
-                    <option value="4" className="option__forform">
-                      BHANVAD
-                    </option>
-                  </select>
-                </form>
-              </div>
-              <div className="select__policestation">
-                <form className="district__police">
-                  <select className="district__police1">
-                    <option value="0" className="option__forform" selected="">
-                      Select Police Station...
-                    </option>
-                    <option value="1" className="option__forform">
-                      KALYANPUR POLICE STATION
-                    </option>
-                    <option value="2" className="option__forform">
-                      OKHA POLICE STATION
-                    </option>
-                    <option value="3" className="option__forform">
-                      KHAMBHALIYA POLICE STATION
-                    </option>
-                    <option value="4" className="option__forform">
-                      BHANVAD POLICE STATION
-                    </option>
-                  </select>
-                </form>
-              </div> */}
-              <form onSubmit={handleSubmit}>
-                <div className="district">
+              {district && (
+                <div className="district__taluko">
                   <select
-                    className="district__1"
-                    value={district}
-                    onChange={(e) => setDistrict(e.target.value)}
+                    className="district__taluko1"
+                    onChange={(e) => setTaluka(e.target.value)}
+                    // disabled={selectedStation}
                   >
-                    <option value="">Select District</option>
-                    {newData.map((dist) => (
-                      <option value={dist.name} className="option__forform">
-                        {dist.name}
-                      </option>
-                    ))}
+                    <option value="">Select Taluka</option>
+                    {dk
+                      ?.find((obj) => obj?.value === district)
+                      ?.talukas?.map((taluko) => (
+                        <option
+                          value={taluko.value}
+                          className="option__forform"
+                        >
+                          {taluko.label}
+                        </option>
+                      ))}
                   </select>
                 </div>
-                {district && (
-                  <div className="district__taluko">
-                    <select
-                      className="district__taluko1"
-                      onChange={(e) => setTaluka(e.target.value)}
-                    >
-                      <option value="">Select Taluka</option>
-                      {newData
-                        ?.find((obj) => obj?.name === district)
-                        ?.taluka?.map((taluko) => (
-                          <option
-                            value={taluko.name}
-                            className="option__forform"
-                          >
-                            {taluko.name}
-                          </option>
-                        ))}
-                    </select>
-                  </div>
-                )}
-                {district && taluka && (
-                  <div className="district__police">
-                    <select
-                      className="district__police1"
-                      onChange={({ target }) => setStation(target.value)}
-                    >
-                      <option value="">Select Station</option>x
-                      {newData
-                        ?.find((obj) => obj?.name === district)
-                        ?.taluka?.find((obj) => obj?.name === taluka)
-                        ?.police_stations?.map((police) => (
-                          <option value={police} className="option__forform">
-                            {police}
-                          </option>
-                        ))}
-                    </select>
-                  </div>
-                )}
-                <button className="sendOtp" disabled={!station}>
-                  Next
-                </button>
-              </form>
+              )}
+              {district && taluka && (
+                <div className="district__police">
+                  <select
+                    className="district__police1"
+                    onChange={({ target }) => setStation(target.value)}
+                    // disabled={selectedStation}
+                  >
+                    <option value="">Select Station</option>x
+                    {dk
+                      ?.find((obj) => obj?.value === district)
+                      ?.talukas?.find((obj) => obj?.value === taluka)
+                      ?.stations?.map((police) => (
+                        <option
+                          value={police.value}
+                          className="option__forform"
+                        >
+                          {police.label}
+                        </option>
+                      ))}
+                  </select>
+                </div>
+              )}
             </div>
+
+            {/* calendar wise  */}
 
             <div className="title">
               <i className="fa fa-solid fa-calendar"></i>
               <span className="text">Select Date </span>
             </div>
+
             <div className="calender">
               <div className="datefrom">
-                <input className="datefrom1" type="date" />
+                <input
+                  className="datefrom1"
+                  type="date"
+                  onChange={(event) => setStartDate(event.target.value)}
+                />
               </div>
               <p className="fordatep">TO</p>
               <div className="dateto">
-                <input className="dateto1" type="date" />
+                <input
+                  className="dateto1"
+                  type="date"
+                  onChange={(event) => setEndDate(event.target.value)}
+                />
               </div>
             </div>
+
             <div className="download_btn btn">
-              <button>Download QR</button>
+              <button>Extract Data</button>
             </div>
 
             <div className="activity-data">
