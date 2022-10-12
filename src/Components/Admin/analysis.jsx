e8008febda109052160139ca2afdf229d84b943a
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-10-12 22:11:23 +0530

    some changes in admin panel

--- src/Components/Admin/analysis.jsx
+++ src/Components/Admin/analysis.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import LineChartConnectNulls from "../../Charts/LineChartConnectNulls";
 
 const Analysis = () => {
   return (
-    <>
-      <div>Analysis</div>
-    </>
+    <div className="analytics">
+      <div class="dash-title">
+        <p>Feedback Management System</p>
+        <hr />
+      </div>
+      <LineChartConnectNulls />
+    </div>
   );
 };
 
